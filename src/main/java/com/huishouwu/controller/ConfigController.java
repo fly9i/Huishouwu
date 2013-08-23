7133f78b3e165aeb2d9edc3c6e5c971f56b9e981
zheng fei <fly9i@163.com>
2013-08-23 08:36:49 +0800

    update some thing

--- src/main/java/com/huishouwu/controller/ConfigController.java
+++ src/main/java/com/huishouwu/controller/ConfigController.java
@@ -2,16 +2,12 @@ package com.huishouwu.controller;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.huishouwu.buss.ConfigHandler;
-import com.huishouwu.dao.ConfigDao;
-import com.huishouwu.dao.UserDao;
 
 @Controller
 @RequestMapping("config")
