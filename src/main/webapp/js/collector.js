9e8e849102307f55e7df8e0fa9ea051395726e40
fly9i <fly9i@>
2013-12-13 18:56:19 +0800

    add all

--- src/main/webapp/js/collector.js
+++ src/main/webapp/js/collector.js
@@ -7,15 +7,19 @@ $(function() {
 			$.ajax({
 				type : "POST",
 				async : true,
-				url : hsw_conf.path + "/user/apply",
-				data : $('#form_register').serialize(),
+				url : hsw_conf.path + "/col/apply",
+				data : $('#form_apply').serialize(),
 				success : function(res) {
-					$("#reg_button").button("reset");
+					$("#apply_corp_button").button("reset");
 					res = eval("(" + res + ")");
 					alert(res.message);
 					if (res.code == 200) {
 						window.location.reload();
 					}
+				},
+				complete:function(res){
+					console.log(res);
+					$("#apply_corp_button").button("reset");
 				}
 			});
 		}
